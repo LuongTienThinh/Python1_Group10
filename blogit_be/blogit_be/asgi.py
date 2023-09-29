b1e4e3038ec3ab3dbbe3d5a663577dae409e128f
LuongTienThinh <ltthinh2001120422@gmail.com>
2023-09-29 08:26:14 +0700

    re config

--- blogit_be/blogit_be/asgi.py
+++ blogit_be/blogit_be/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for blogIt project.
+ASGI config for blogit_be project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogIt.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogit_be.settings")
 
 application = get_asgi_application()
